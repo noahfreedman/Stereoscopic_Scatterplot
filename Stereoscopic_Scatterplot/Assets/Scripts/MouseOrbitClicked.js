8bc98eb570751c8283d8f8c952e533d65bd3e53d
EricPug <EricPug@gmail.com>
2013-11-21 18:12:48 -0800

    commandline commit try 2

---
+++ Stereoscopic_Scatterplot/Assets/Scripts/MouseOrbitClicked.js
@@ -0,0 +1,71 @@
+// Orbit to point at a given target GameObject transform
+// rotate when mouse button pressed
+// xoom in/out with mouse wheel
+// - Eric Grosser.
+
+
+var target : Transform;
+var distance = 1.0;
+
+var xSpeed = 250.0;
+var ySpeed = 120.0;
+
+var yMinLimit = -20;
+var yMaxLimit = 80;
+
+private var x = 0.0;
+private var y = 0.0;
+public var mouseButtonNumber = 0;
+
+private var  cameraDistanceMax = 6f;
+private var  cameraDistanceMin = 0.7f;
+private var  scrollSpeed = 1.0f;
+
+
+
+@script AddComponentMenu("Camera-Control/Mouse Orbit")
+
+function Start () {
+    var angles = transform.eulerAngles;
+    x = angles.y;
+    y = angles.x;
+
+	// Make the rigid body not change rotation
+   	if (rigidbody)
+		rigidbody.freezeRotation = true;
+}
+
+function LateUpdate () {
+	if (target && Input.GetMouseButton(mouseButtonNumber)) {
+        x += Input.GetAxis("Mouse X") * xSpeed * 0.02;
+        y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02;
+ 		
+ 		y = ClampAngle(y, yMinLimit, yMaxLimit);
+ 		       
+        var rotation = Quaternion.Euler(y, x, 0);
+        var position = rotation * Vector3(0.0, 0.0, -distance) + target.position;
+        
+        transform.rotation = rotation;
+        transform.position = position;
+  
+    }
+	if (Input.GetAxis("Mouse ScrollWheel") != 0) {
+        
+  		distance += Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+
+		distance = Mathf.Clamp(distance, cameraDistanceMin, cameraDistanceMax);
+        rotation = Quaternion.Euler(y, x, 0);
+        position = rotation * Vector3(0.0, 0.0, -distance) + target.position;
+        
+		transform.rotation = rotation;
+        transform.position = position;
+	}
+}
+
+static function ClampAngle (angle : float, min : float, max : float) {
+	if (angle < -360)
+		angle += 360;
+	if (angle > 360)
+		angle -= 360;
+	return Mathf.Clamp (angle, min, max);
+}
