c73d26f9387647e6ae95e59b42904e011b83c464
EricPug <EricPug@gmail.com>
2013-12-11 09:41:17 -0800

    Selected objects menu delete menu Material added to lines

--- Stereoscopic_Scatterplot/Assets/Scripts/Inventory.cs
+++ Stereoscopic_Scatterplot/Assets/Scripts/Inventory.cs
@@ -6,92 +6,130 @@ using System.Collections;
 // delete
 // show edit sub menus (??)
 public class Inventory : MonoBehaviour
-{	
-		public bool ShowMenu = true;
-		public float MenuAreaWidth = 200;
-		private float areaHeight = 100;
-		public float ScreenXOffset = 10;
-		public bool doubleMenu = false; // Main menu controls this
-		private GameObject CurrentSelection;
-		private Rect MenuAreaRect;
-		private Rect MenuAreaRightScreen;
-		private Rect fullScreenRect = new Rect (0, 0, Screen.width, Screen.height);
-		private Rect MenuRect = new Rect (0, 30, 130, 300);
-		private Color MenuDefaultGUIColor;
-		public Color SelectedColor = Color.green;
+{
+    public bool ShowMenu = true;
+    public float MenuAreaWidth = 200;
+    private float areaHeight = 100;
+    public float ScreenXOffset = 10;
+    public bool doubleMenu = false; // Main menu controls this
+    private GameObject CurrentSelection;
+    private Rect MenuAreaRect;
+    private Rect MenuAreaRightScreen;
+    private Rect fullScreenRect = new Rect(0, 0, Screen.width, Screen.height);
+    private Rect MenuRect = new Rect(0, 30, 130, 300);
+    private Color MenuDefaultGUIColor;
+    public Color SelectedColor = Color.green;
+    private bool ShowSelectedObjectMenu = false; //toggle showing the pop up menu
 
-		void Start ()
-		{
-				MenuDefaultGUIColor = GUI.contentColor;
+    void Start()
+    {
+        MenuDefaultGUIColor = GUI.contentColor;
 
-				ScreenXOffset = (Screen.width / 2) - ScreenXOffset;
+        ScreenXOffset = (Screen.width / 2) - ScreenXOffset;
 
-				areaHeight = Screen.height;
-				MenuAreaRect = new Rect (ScreenXOffset, 0, MenuAreaWidth, areaHeight);
-				MenuAreaRightScreen = new Rect (Screen.width, 0, MenuAreaWidth, areaHeight);
+        areaHeight = Screen.height;
+        MenuAreaRect = new Rect(ScreenXOffset, 0, MenuAreaWidth, areaHeight);
+        MenuAreaRightScreen = new Rect(Screen.width, 0, MenuAreaWidth, areaHeight);
 
-		}
+    }
 
-		void OnGUI ()
-		{
+    void OnGUI()
+    {
 
-				GUILayout.BeginArea (MenuAreaRect);
+        GUILayout.BeginArea(MenuAreaRect);
 
-				DisplayGUI ();
-				GUILayout.EndArea ();
-				if (doubleMenu) {
+        DisplayGUI();
+        GUILayout.EndArea();
+        if (doubleMenu)
+        {
 
-						GUILayout.BeginArea (MenuAreaRightScreen);
-						DisplayGUI ();
-						GUILayout.EndArea ();
-				}
-		}
+            GUILayout.BeginArea(MenuAreaRightScreen);
+            DisplayGUI();
+            GUILayout.EndArea();
+        }
+    }
+    void MenuDisplaySelectedObject(GameObject obj)
+    {
+        GUI.enabled = true;
+        GUILayout.BeginVertical("box");
+        GUILayout.Label(obj.name);
+        if (GUILayout.Button("delete"))
+        {
+            Destroy(obj);
+        }
+        GUILayout.EndHorizontal();
+    }
+    void SelectObject(GameObject obj)
+    {
+        if (CurrentSelection)
+        {
+            if (CurrentSelection.GetInstanceID() == obj.GetInstanceID())
+            {
+                //toggle selection off when selected twice.
+                CurrentSelection = null;
+            }
+            else
+            {
+                CurrentSelection = obj;
+            }
+        }
+        else
+        {
+            CurrentSelection = obj;
+        }
+    }
 
-		void SelectObject (GameObject obj)
-		{
-				if (CurrentSelection) {
-						if (CurrentSelection.GetInstanceID () == obj.GetInstanceID ()) {
-								//toggle selection off when selected twice.
-								CurrentSelection = null;
-						} else {
-								CurrentSelection = obj;
-						}
-				} else {
-						CurrentSelection = obj;
-				}
-		}
+    Color CurrentButtonColor(GameObject obj)
+    {
+        if (CurrentSelection)
+        {
+            if (CurrentSelection.GetInstanceID() == obj.GetInstanceID())
+            {
+                return SelectedColor;
+            }
+            else
+            {
+                return MenuDefaultGUIColor;
+            }
+        }
+        else
+        {
+            return MenuDefaultGUIColor;
+        }
+    }
 
-		Color CurrentButtonColor (GameObject obj)
-		{
-				if (CurrentSelection) {
-						if (CurrentSelection.GetInstanceID () == obj.GetInstanceID ()) {					
-								return SelectedColor;
-						} else {
-								return MenuDefaultGUIColor;
-						}
-				} else {
-						return MenuDefaultGUIColor;
-				}
-		}
+    void DisplayGUI()
+    {
+        if (ShowMenu)
+        {
+            ShowSelectedObjectMenu = (CurrentSelection != null);
+            GUILayout.BeginHorizontal();
+            if (ShowSelectedObjectMenu)
+            {
 
-		void DisplayGUI ()
-		{
-				if (ShowMenu) {
-						GUILayout.BeginVertical ("box");
-						if (transform.childCount > 0) {
-								foreach (Transform child in transform) {
-										GUILayout.BeginHorizontal ();
-										GUI.contentColor = CurrentButtonColor (child.gameObject);
-										if (GUILayout.Button (child.name)) {
-												SelectObject (child.gameObject);
-										}
-										GUI.contentColor = MenuDefaultGUIColor;
-										GUILayout.EndHorizontal ();
-								}
-						} else {
-								GUILayout.Label ("0 Objects");
-						}
-						GUILayout.EndVertical ();
-				} 
-		}
+                MenuDisplaySelectedObject(CurrentSelection);
+            }
+            GUILayout.BeginVertical("box");
+            if (transform.childCount > 0)
+            {
+                foreach (Transform child in transform)
+                {
+                    GUILayout.BeginHorizontal();
+                    GUI.contentColor = CurrentButtonColor(child.gameObject);
+                    if (GUILayout.Button(child.name))
+                    {
+                        SelectObject(child.gameObject);
+                    }
+                    GUI.contentColor = MenuDefaultGUIColor;
+                    GUILayout.EndHorizontal();
+                }
+            }
+            else
+            {
+                GUILayout.Label("0 Objects");
+            }
+            GUILayout.EndVertical();
+            GUILayout.EndHorizontal();
+        }
+    }
 }
